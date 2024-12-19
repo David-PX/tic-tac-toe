457ef8f30c01ac07980bb8778543f5fe4b3f8374
David-Gcs <82048609+David-Gcs@users.noreply.github.com>
2024-12-18 20:37:00 -0400

    Persist game state in local storage for board and turn

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,16 @@ import { checkWinnerFrom, checkEndGame } from './logic/board.js'
 import { Board } from './components/Board.jsx'
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  });
+  
 
-  const [turn, setTurn] = useState(TURNS.X)
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ? JSON.parse(turnFromStorage) : TURNS.X
+  });
   // null there is no winner
   // false there is a tie
   // true there is a winner
@@ -27,6 +34,9 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
 
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', JSON.stringify(newTurn))
+
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
       confetti();
@@ -40,6 +50,9 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   return (
